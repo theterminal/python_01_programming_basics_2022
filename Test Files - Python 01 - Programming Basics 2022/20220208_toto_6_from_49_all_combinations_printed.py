9f1e6981b8d74e450aa26cbf2bfad0ba8bd1d806
theterminal <kirilk777@gmail.com>
2022-06-01 19:52:16 +0300

    Initial commit

---
+++ Test Files - Python 01 - Programming Basics 2022/20220208_toto_6_from_49_all_combinations_printed.py
@@ -0,0 +1,14 @@
+# 20220208 - Python Code - TOTO 6 from 49 All Combinations Printed
+
+# given information
+num_cycle = 0
+
+# calculations & result
+for i in range(1, 50):
+    for j in range(1, 50):
+        for k in range(1, 50):
+            for l in range(1, 50):
+                for m in range(1, 50):
+                    for n in range(1, 50):
+                        num_cycle += 1
+                        print(f"{i}, {j}, {k}, {l}, {m}, {n}; combination # {num_cycle}")
