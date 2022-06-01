17665bdf5133a07601cc7421b26d2ae64ae0891c
theterminal <kirilk777@gmail.com>
2022-06-01 19:02:21 +0300

    Initial commit

---
+++ 20220207 - more_exercises - 05/03_stream_of_letters.py
@@ -0,0 +1,31 @@
+# 20220206 - Python Code
+# Stream of Letters
+
+# user's input
+letter_entered = input()                            # ASCII from 65 to 90 & from 97 to 122
+
+# additional elements
+word, word_print = '', ''
+count_c = count_o = count_n = 0
+
+# result
+while letter_entered != "End":
+
+    if letter_entered == "c" and count_c == 0:
+        count_c = 1
+    elif letter_entered == "o" and count_o == 0:
+        count_o = 1
+    elif letter_entered == "n" and count_n == 0:
+        count_n = 1
+    else:
+        if 65 <= ord(letter_entered) <= 90 or 97 <= ord(letter_entered) <= 122:
+            word += letter_entered
+
+    if (count_c + count_o + count_n) == 3:
+        word = word + " "
+        count_c = count_o = count_n = 0
+        word_print = word
+
+    letter_entered = input()
+
+print(word_print)
