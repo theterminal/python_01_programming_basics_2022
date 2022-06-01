76c1bd1a1c4bf7c1ff5601a62331dbeb75b3a74d
theterminal <kirilk777@gmail.com>
2022-06-01 18:58:26 +0300

    Initial commit

---
+++ 20220207 - more_exercises - 05/02_report_system.py
@@ -0,0 +1,45 @@
+# 20220206 - Python Code
+# Report System
+
+# user's input
+money_expected_to_be_raised = int(input())
+price_entered = input()
+
+# additional elements
+counter = money_received_total = money_received_cash = money_received_cc = counter_cash = counter_cc = 0
+
+# calculations & result
+while price_entered != "End":
+    price_entered = int(price_entered)
+    counter += 1
+
+    if counter % 2 != 0:                                # cash transactions
+        if price_entered > 100:
+            print("Error in transaction!")
+        else:
+            print("Product sold!")
+            money_received_cash += price_entered
+            counter_cash += 1
+    else:                                               # card transactions
+        if price_entered < 10:
+            print("Error in transaction!")
+        else:
+            print("Product sold!")
+            money_received_cc += price_entered
+            counter_cc += 1
+
+    money_received_total = money_received_cash + money_received_cc
+
+    if money_received_total >= money_expected_to_be_raised:
+        break
+
+    price_entered = input()
+
+if price_entered == "End":
+    print("Failed to collect required money for charity.")
+    quit()
+
+average_cash = money_received_cash / counter_cash
+average_cc = money_received_cc / counter_cc
+print(f"Average CS: {average_cash:.2f}")
+print(f"Average CC: {average_cc:.2f}")
