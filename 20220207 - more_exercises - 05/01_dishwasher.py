91d7300db3de2f638724dbdd455b0061c5028df0
theterminal <kirilk777@gmail.com>
2022-06-01 18:48:15 +0300

    Initial commit

---
+++ 20220207 - more_exercises - 05/01_dishwasher.py
@@ -0,0 +1,40 @@
+# 20220205 - Python Code
+# Dishwasher
+
+# user's input
+num_bottles_dish_liquid_used = int(input())
+num_dishes_to_wash = input()
+
+# additional elements
+cycle = num_pots = num_dishes = detergent = 0
+
+# calculations
+total_detergent_ml = num_bottles_dish_liquid_used * 750
+
+# result
+while num_dishes_to_wash != "End":
+    num_dishes_to_wash = int(num_dishes_to_wash)
+    cycle += 1
+
+    if cycle % 3 == 0:
+        num_pots += num_dishes_to_wash
+        detergent_pots = num_dishes_to_wash * 15
+        detergent += detergent_pots
+    else:
+        num_dishes += num_dishes_to_wash
+        detergent_dishes = num_dishes_to_wash * 5
+        detergent += detergent_dishes
+
+    if detergent > total_detergent_ml:
+        diff = abs(detergent - total_detergent_ml)
+        print(f"Not enough detergent, {diff} ml. more necessary!")
+        quit()
+
+    num_dishes_to_wash = input()
+
+    if num_dishes_to_wash == "End":
+        break
+
+print(f"Detergent was enough!")
+print(f"{num_dishes} dishes and {num_pots} pots were washed.")
+print(f"Leftover detergent {total_detergent_ml - detergent} ml.")
