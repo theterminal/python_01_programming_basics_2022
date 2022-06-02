14ba396de37a3bcd9c24abae100d3fe23c1729c3
theterminal <kirilk777@gmail.com>
2022-06-02 12:01:23 +0300

    Initial commit.

---
+++ 20220213 - 12 - E - Nested Loops/04_train_the_trainers.py
@@ -0,0 +1,24 @@
+# 20220210 - Python Code - Train The Trainers
+
+# user's input
+num_of_jury_members = int(input())
+title_of_presentation = input()
+
+# given information
+sum_grades, grade_entered, sum_of_average_grades, counter_titles = 0, 0, 0, 0
+
+# calculations & result
+while title_of_presentation != "Finish":
+    for current_jury_member in range(num_of_jury_members):
+        grade_entered = float(input())
+        sum_grades += grade_entered
+
+    average_grade = sum_grades / num_of_jury_members
+    print(F"{title_of_presentation} - {average_grade:.2f}.")
+    sum_of_average_grades += average_grade
+    counter_titles += 1
+    grade_entered = sum_grades = 0
+    title_of_presentation = input()
+
+sum_of_average_grades = sum_of_average_grades / counter_titles
+print(f"Student's final assessment is {sum_of_average_grades:.2f}.")
