333c515563f1f37f9eb93ba8278ccd490e2f92cd
theterminal <kirilk777@gmail.com>
2022-06-02 15:03:32 +0300

    Initial commit.

---
+++ 20220213 - 12 - E - Nested Loops/notes_02_prime_number_generator.py
@@ -0,0 +1,24 @@
+# 20220210 - Python Code - Prime Number Generator
+
+range_start = int(input("Enter start range number: "))
+range_end = int(input("Enter end range number: "))
+flag_prime = 'prime'
+prime_counter = 0
+num_to_print = ''
+
+for num in range(range_start, range_end + 1):
+    while True:
+        flag_prime = "prime"
+
+        for i in range(2, num):
+            if num % i == 0:
+                flag_prime = "non prime"
+
+        if flag_prime == "prime" and num > 1:
+            num_to_print += (str(num) + ', ')
+            prime_counter += 1
+
+        break
+
+print(f'\nThe prime numbers are:\n{num_to_print.strip(", ")}')
+print(f"\nAll prime numbers in the range ({range_start} : {range_end}) are {prime_counter}")
