25d430394a8da8e367f5378ebce6d1ed0c95aa0a
theterminal <kirilk777@gmail.com>
2022-06-02 11:41:37 +0300

    Initial commit

---
+++ 20220213 - 12 - E - Nested Loops/02_equal_sums_even_odd_position.py
@@ -0,0 +1,24 @@
+# Python Code - Equal Sums Even Odd Position
+
+# user's input
+num_1_entered = int(input())
+num_2_entered = int(input())
+
+if num_1_entered > num_2_entered:
+    num_temp = num_1_entered
+    num_1_entered = num_2_entered
+    num_2_entered = num_temp
+
+# calculations & result
+for num in range(num_1_entered, num_2_entered + 1):
+    num_to_str = str(num)
+    sum_num_on_even_position = sum_num_on_odd_position = 0
+
+    for index, digit in enumerate(num_to_str):
+        if index % 2 == 0:
+            sum_num_on_odd_position += int(digit)
+        else:
+            sum_num_on_even_position += int(digit)
+
+    if sum_num_on_even_position == sum_num_on_odd_position:
+        print(num, end=" ")
