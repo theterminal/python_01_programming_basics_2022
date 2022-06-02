2ec637dd551657e1d4926a110b5e705260ec19ed
theterminal <kirilk777@gmail.com>
2022-06-02 21:31:26 +0300

    Refactored starting 3 variables.

---
+++ 20220214 - more_exercises - 06/03_lucky_numbers.py
@@ -0,0 +1,10 @@
+# 20220215 - Python Code - Lucky Numbers - judge url: https://judge.softuni.org/Contests/Practice/Index/1381#2
+
+num_N = int(input())
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            for m in range(1, 10):
+                if (i + j == k + m) and num_N % (i + j) == 0:
+                    print(f'{i}{j}{k}{m}', end=' ')
