8f39cd2ef34122897ae968294d7b832dbf2f7ed8
theterminal <kirilk777@gmail.com>
2022-06-02 15:03:01 +0300

    Initial commit.

---
+++ 20220213 - 12 - E - Nested Loops/notes_01_strings.py
@@ -0,0 +1,9 @@
+# 20220210 - Python Code - examples
+
+# result
+words = ('apple', 'banana', 'lemon', 'apple')
+
+print("words = ('apple', 'banana', 'lemon', 'apple')")
+print(f"result of print(words) is: {words}")
+print(f"result of print(list(words)) is: {list(words)}")
+print(f"result of print(list(enumerate(words))) is: {list(enumerate(words))})")
