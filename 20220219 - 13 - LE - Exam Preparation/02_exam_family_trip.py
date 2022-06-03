5a6fa8b510adba8501a20b36ffbd1dcdd7ea0ae1
theterminal <kirilk777@gmail.com>
2022-06-03 07:30:55 +0300

    Initial commit

---
+++ 20220219 - 13 - LE - Exam Preparation/02_exam_family_trip.py
@@ -0,0 +1,22 @@
+# Python Code - Family Trip - https://judge.softuni.org/Contests/Practice/Index/1745#1
+
+# user input
+budget_entered = float(input())
+num_nights_entered = int(input())
+price_per_night = float(input())
+percent_additional_expenses = int(input()) * 0.01
+
+# calculations & results
+if num_nights_entered > 7:
+    price_per_night *= 0.95
+
+amount_hotel = num_nights_entered * price_per_night
+additional_expenses = percent_additional_expenses * budget_entered
+
+total_needed = amount_hotel + additional_expenses
+diff = abs(budget_entered - total_needed)
+
+if budget_entered >= total_needed:
+    print(f"Ivanovi will be left with {diff:.2f} leva after vacation.")
+else:
+    print(f"{diff:.2f} leva needed.")
