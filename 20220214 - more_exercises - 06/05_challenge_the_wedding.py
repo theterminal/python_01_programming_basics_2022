6443689b21a8d0a452a2467759b6b75a4cf7699d
theterminal <kirilk777@gmail.com>
2022-06-03 07:10:08 +0300

    Initial commit

---
+++ 20220214 - more_exercises - 06/05_challenge_the_wedding.py
@@ -0,0 +1,19 @@
+# 20220215 - Python Code - Challenge The Wedding - judge url: https://judge.softuni.org/Contests/Practice/Index/1381#4
+
+num_customers_men = int(input())
+num_customers_women = int(input())
+num_tables_max = int(input())
+
+counter_tables, flag = 0, 0
+
+for i in range(1, num_customers_men + 1):
+    for j in range(1, num_customers_women + 1):
+        counter_tables += 1
+
+        if counter_tables > num_tables_max:
+            flag = 1
+            break
+        print(f'({i} <-> {j})', end=" ")
+
+    if flag:
+        break
