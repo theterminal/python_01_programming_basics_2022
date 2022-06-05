5e62764bb1c135d15951a01c35f9bbb901abdbd6
theterminal <kirilk777@gmail.com>
2022-06-05 13:06:46 +0300

    Initial commit

---
+++ 20220219 - 14 - Python - Pre-Exam/20220219 - Python - Pre-Exam Problems/03_excursion_calculator.py
@@ -0,0 +1,34 @@
+# 20220219 - Python Code - Excursion Calculator - judge url: https://judge.softuni.org/Contests/Compete/Index/3351#2
+
+# user information
+num_people_entered = int(input())
+season_entered = input()
+
+# calculations & results
+price = 0
+
+if season_entered == "spring":
+    if num_people_entered <= 5:
+        price = 50.00
+    else:
+        price = 48.00
+elif season_entered == "summer":
+    if num_people_entered <= 5:
+        price = 48.50
+    else:
+        price = 45.00
+    price *= 0.85
+elif season_entered == "autumn":
+    if num_people_entered <= 5:
+        price = 60.00
+    else:
+        price = 49.50
+elif season_entered == "winter":
+    if num_people_entered <= 5:
+        price = 86.00
+    else:
+        price = 85.00
+    price *= 1.08
+
+price_total = num_people_entered * price
+print(f"{price_total:.2f} leva.")
