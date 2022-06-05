aa3555dc1f9cdde3940d4b7779c4949f22a4c299
theterminal <kirilk777@gmail.com>
2022-06-05 13:19:39 +0300

    Initial commit

---
+++ 20220219 - 14 - Python - Pre-Exam/20220219 - Python - Pre-Exam Problems/05_excursion_sale.py
@@ -0,0 +1,31 @@
+# 20220219 - Python Code - Excursion Sale - judge url: https://judge.softuni.org/Contests/Compete/Index/3351#4
+
+# user information
+num_sea_vac = int(input())
+num_mountain_vac = int(input())
+
+vac_pack_entered = input()
+
+# calculations & results
+profit = 0
+
+while vac_pack_entered != "Stop":
+    if vac_pack_entered == "sea" and num_sea_vac > 0:
+        profit += 680
+        num_sea_vac -= 1
+    elif vac_pack_entered == "see":
+        profit += 0
+    elif vac_pack_entered == "mountain" and num_mountain_vac > 0:
+        profit += 499
+        num_mountain_vac -= 1
+    elif vac_pack_entered == "mountain":
+        profit += 0
+
+    if num_sea_vac == 0 and num_mountain_vac == 0:
+        print(f"Good job! Everything is sold.")
+        print(f"Profit: {profit} leva.")
+        quit()
+
+    vac_pack_entered = input()
+
+print(f"Profit: {profit} leva.")
