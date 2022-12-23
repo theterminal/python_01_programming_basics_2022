6d54f8f88619bedba2037f4e39358abded1088d9
theterminal <kirilk777@gmail.com>
2022-12-23 19:34:28 +0200

    Refactored

--- 20220108 - 01 - L - First Steps In Coding/07_projects_creation.py
+++ 20220108 - 01 - L - First Steps In Coding/07_projects_creation.py
@@ -1,13 +1,16 @@
-# 20220108 - Python Code - Projects Creation
-# judge url: https://judge.softuni.org/Contests/Compete/Index/2423#6
-# pastebin url: https://pastebin.com/zvHNfX62
+# 20220108 - Python - Programming Basics
+# 07 - Projects Creation - judge: https://judge.softuni.org/Contests/Compete/Index/2423#6
+# pastebin: https://pastebin.com/zvHNfX62
+
 
 # user input
 name = input()
 num_of_projects = int(input())
 
+
 # calculations
 hours_needed = num_of_projects * 3
+
 
 # output
 print(f"The architect {name} will need {hours_needed} hours to complete {num_of_projects} project/s.")
