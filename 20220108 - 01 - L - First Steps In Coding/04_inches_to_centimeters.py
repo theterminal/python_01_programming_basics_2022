3627a13cb703ce1d0111d5257ea5910effa336fe
theterminal <kirilk777@gmail.com>
2022-12-23 19:34:11 +0200

    Refactored

--- 20220108 - 01 - L - First Steps In Coding/04_inches_to_centimeters.py
+++ 20220108 - 01 - L - First Steps In Coding/04_inches_to_centimeters.py
@@ -1,9 +1,8 @@
-# 20220108 Python Code - Converting inches ito centimeters
-# judge url: https://judge.softuni.org/Contests/Compete/Index/2423#3
-# pastebin url: https://pastebin.com/bMe1ieiG
+# 20220108 - Python - Programming Basics
+# 04 - Converting inches ito centimeters - judge: https://judge.softuni.org/Contests/Compete/Index/2423#3
+# pastebin: https://pastebin.com/bMe1ieiG
+
 
 inches_entered = float(input())
-
 centimeters = inches_entered * 2.54
-
 print(centimeters)
