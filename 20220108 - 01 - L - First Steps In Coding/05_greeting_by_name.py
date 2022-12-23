3e48ed8f13b959d4f518b77bb7f84f9b9f88ad9a
theterminal <kirilk777@gmail.com>
2022-12-23 19:34:17 +0200

    Refactored

--- 20220108 - 01 - L - First Steps In Coding/05_greeting_by_name.py
+++ 20220108 - 01 - L - First Steps In Coding/05_greeting_by_name.py
@@ -1,13 +1,16 @@
-# 20220108 Python Code - Greeting By Name
-# judge url: https://judge.softuni.org/Contests/Compete/Index/2423#4
-# pastebin url: https://pastebin.com/6WDEm0NV
+# 20220108 - Python - Programming Basics
+# 05 - Greeting By Name - judge: https://judge.softuni.org/Contests/Compete/Index/2423#4
+# pastebin: https://pastebin.com/6WDEm0NV
+
 
 # ------------------ version 1 --------------------
 
-name = input()
 
+name = input()
 print("Hello, " + name + "!")
+
 
 # ------------------ version 2 ---------------------
 
-# print(f'Hello, {name}!')  # f-string format for same result
+
+print(f'Hello, {name}!')                # f-string format for same result
