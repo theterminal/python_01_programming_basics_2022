377191527c225ffdbf4538326e49d3dbed5878ba
theterminal <kirilk777@gmail.com>
2022-12-24 08:58:43 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/08_basketball_equipment.py
+++ 20220109_02_E_First_Steps_In_Coding/08_basketball_equipment.py
@@ -1,9 +1,11 @@
-# 20220109 - Python Code - Basketball Equipment
-# judge url: https://judge.softuni.org/Contests/Compete/Index/2424#7
-# pastebin url: https://pastebin.com/SDxDvKqy
+# 20220109 - Python - Programming Basics
+# 08 - Basketball Equipment - judge: https://judge.softuni.org/Contests/Compete/Index/2424#7
+# pastebin: https://pastebin.com/SDxDvKqy
+
 
 # user input
 yearly_fee = int(input())
+
 
 # calculations
 cost_of_shoes = yearly_fee * 0.6
@@ -12,6 +14,7 @@ cost_of_ball = cost_of_cloths * 0.25
 cost_of_accessories = cost_of_ball * 0.2
 sub_total = cost_of_shoes + cost_of_cloths + cost_of_ball + cost_of_accessories
 total = sub_total + yearly_fee
+
 
 # output
 print(f"{total}")
