7ba808f430b2e0a8cef4c6dd32305ba78f0459f5
theterminal <kirilk777@gmail.com>
2022-12-24 08:56:55 +0200

    Refactored

---
+++ 20220109_02_E_First_Steps_In_Coding/02_radians_to_degrees.py
@@ -0,0 +1,17 @@
+# 20220109 - Python - Programming Basics
+# 02 - Radians to Degrees - judge: https://judge.softuni.org/Contests/Compete/Index/2424#1
+# pastebin: https://pastebin.com/drKjaV3g
+
+import math
+
+
+# user input
+radians_entered = float(input())
+
+
+# calculations
+degrees = radians_entered * 180 / math.pi
+
+
+# output
+print(degrees)
