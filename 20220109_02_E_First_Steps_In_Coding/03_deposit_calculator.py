0b942215c7cc92ffbd6fa2e7c60470acc6d603be
theterminal <kirilk777@gmail.com>
2022-12-24 08:57:09 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/03_deposit_calculator.py
+++ 20220109_02_E_First_Steps_In_Coding/03_deposit_calculator.py
@@ -1,16 +1,19 @@
-# 20220109 - Python Code - Deposit Calculator
-# judge url: https://judge.softuni.org/Contests/Compete/Index/2424#2
-# pastebin url: https://pastebin.com/1DzyN460
+# 20220109 - Python - Programming Basics
+# 03 - Deposit Calculator - judge: https://judge.softuni.org/Contests/Compete/Index/2424#2
+# pastebin: https://pastebin.com/1DzyN460
+
 
 # user input
 amount_deposited = float(input())
 term_in_months = int(input())
 yearly_APR = float(input())
 
+
 # calculations
 real_APR_for_1_m = ((yearly_APR / 100) / 12)
 interest = amount_deposited * real_APR_for_1_m * term_in_months
 total_amount = amount_deposited + interest
+
 
 # output
 print(total_amount)
