9c9d9b319ef9b6a4f86e6714834f04ccecb97091
theterminal <kirilk777@gmail.com>
2022-12-24 08:56:15 +0200

    Refactored

---
+++ 20220109_02_E_First_Steps_In_Coding/01_usd_to_bgn.py
@@ -0,0 +1,15 @@
+# 20220109 - Python - Programming Basics
+# 01 - USD to BGN Converter - judge: https://judge.softuni.org/Contests/Compete/Index/2424#0
+# pastebin: https://pastebin.com/Lbwpxxdy
+
+
+# User input
+usd_to_convert = float(input())
+
+
+# Calculations
+bgn_converted_from_usd = usd_to_convert * 1.79549
+
+
+# output
+print(bgn_converted_from_usd)
