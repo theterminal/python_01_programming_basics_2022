917708fcbd5c30bb8ea3de6d10bf17c6f52647ce
theterminal <kirilk777@gmail.com>
2022-12-24 09:00:04 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/additional_01_trapezoid_area.py
+++ 20220109_02_E_First_Steps_In_Coding/additional_01_trapezoid_area.py
@@ -1,14 +1,17 @@
-# 20220111 - Python Code - Trapezoid Area
-# judge url: https://judge.softuni.org/Contests/Practice/Index/1642#0
-# pastebin url: https://pastebin.com/00RLHYpa
+# 20220111 - Python - Programming Basics
+# additional 01 - Trapezoid Area - judge: https://judge.softuni.org/Contests/Practice/Index/1642#0
+# pastebin: https://pastebin.com/00RLHYpa
+
 
 # user input
 base_a = float(input())
 base_b = float(input())
 height = float(input())
 
+
 # calculations
 area = (base_a + base_b) * height / 2
+
 
 # output
 print(f"{area:.2f}")
