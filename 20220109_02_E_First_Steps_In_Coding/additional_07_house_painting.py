d4299399c887455c042431cd47dd566554d49e44
theterminal <kirilk777@gmail.com>
2022-12-25 08:32:56 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/additional_07_house_painting.py
+++ 20220109_02_E_First_Steps_In_Coding/additional_07_house_painting.py
@@ -1,11 +1,13 @@
-# 20220111 - Python Code - House Painting
-# judge url: https://judge.softuni.org/Contests/Practice/Index/1642#6
-# pastebin url: https://pastebin.com/LArWAR7a
+# 20220111 - Python - Programming Basics
+# additional 07 - House Painting - judge: https://judge.softuni.org/Contests/Practice/Index/1642#6
+# pastebin: https://pastebin.com/LArWAR7a
+
 
 # user input
 house_height_m = float(input())
 house_width_m = float(input())
 roof_height_m = float(input())
+
 
 # calculations
 walls_short_sq_m = (house_height_m ** 2) * 2 - (2 * 1.2)
@@ -20,18 +22,21 @@ total_roof_sq_m = roof_rectangles_sq_m + roof_triangles_sq_m
 
 red_paint_l = total_roof_sq_m / 4.3
 
+
 # output
 print('{:.{}f}'.format(green_paint_l, 2))
 print('{:.{}f}'.format(red_paint_l, 2))
 
+
 # -------------------------- first solution---------------------------------------
 
-# x = 6
-# y = 10
-# h = 5.2
-#
-# walls = (((2 * x * x) - (2 * 1.2)) + 2 * ((x * y) - (1.5 * 1.5))) / 3.4
-# roof = (2 * ((x * y) + (x * h / 2))) / 4.3
-#
-# print(f"Green paint: {walls:.2f}")
-# print(f"Red paint: {roof:.2f}")
+
+x = 6
+y = 10
+h = 5.2
+
+walls = (((2 * x * x) - (2 * 1.2)) + 2 * ((x * y) - (1.5 * 1.5))) / 3.4
+roof = (2 * ((x * y) + (x * h / 2))) / 4.3
+
+print(f"Green paint: {walls:.2f}")
+print(f"Red paint: {roof:.2f}")
