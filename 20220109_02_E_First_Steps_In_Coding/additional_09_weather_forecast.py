98c08921555970db22c0ea089102b32f0a7115d5
theterminal <kirilk777@gmail.com>
2022-12-25 08:33:06 +0200

    Refactored

---
+++ 20220109_02_E_First_Steps_In_Coding/additional_09_weather_forecast.py
@@ -0,0 +1,14 @@
+# 20220111 - Python - Programming Basics
+# additional 09 - Weather Forecast - judge: https://judge.softuni.org/Contests/Practice/Index/1642#8
+# pastebin: https://pastebin.com/vaKaRQXw
+
+
+# user input
+weather = input()
+
+
+# calculations
+if weather == "sunny":
+    print("It's warm outside!")
+else:
+    print("It's cold outside!")
