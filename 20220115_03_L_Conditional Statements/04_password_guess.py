43480c796996e92cca9769aba9ca81529a5afb91
theterminal <kirilk777@gmail.com>
2022-12-25 08:42:25 +0200

    Refactored

--- 20220115_03_L_Conditional Statements/04_password_guess.py
+++ 20220115_03_L_Conditional Statements/04_password_guess.py
@@ -1,7 +1,10 @@
-# 20220112 - Python Code - Password guess
+# 20220112 - Python - Programming Basics - Conditional Statements
+# 04 - Password guess
+
 
 # user input
 password_entered = input()
+
 
 # calculations & results
 if password_entered == "s3cr3t!P@ssw0rd":
