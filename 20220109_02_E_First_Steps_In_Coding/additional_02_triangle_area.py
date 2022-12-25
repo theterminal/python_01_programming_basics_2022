ce19c2bd1101d09216a501b5dea4202c8d3fb27a
theterminal <kirilk777@gmail.com>
2022-12-25 08:32:16 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/additional_02_triangle_area.py
+++ 20220109_02_E_First_Steps_In_Coding/additional_02_triangle_area.py
@@ -1,13 +1,16 @@
-# 20220111 - Python Code - Triangle Area
-# judge url: https://judge.softuni.org/Contests/Practice/Index/1642#1
-# pastebin url: https://pastebin.com/fNPkQfpB
+# 20220111 - Python - Programming Basics
+# additional 02 - Triangle Area - judge: https://judge.softuni.org/Contests/Practice/Index/1642#1
+# pastebin: https://pastebin.com/fNPkQfpB
+
 
 # user input
 side_a = float(input())
 height = float(input())
 
+
 # calculations
 area = side_a * height / 2
+
 
 # output
 print(f"{area:.2f}")
