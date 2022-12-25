ad58cca1b0d45f4ca12ae5da9de479cddb27e81e
theterminal <kirilk777@gmail.com>
2022-12-25 08:42:04 +0200

    Refactored

--- 20220115_03_L_Conditional Statements/02_greater_number.py
+++ 20220115_03_L_Conditional Statements/02_greater_number.py
@@ -1,8 +1,11 @@
-# 20220112 - Python Code - Greater Number
+# 20220112 - Python - Programming Basics - Conditional Statements
+# 02 - Greater Number
+
 
 # user input
 number_1 = int(input())
 number_2 = int(input())
+
 
 # calculations & results
 if number_1 > number_2:
