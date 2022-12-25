5a712269f1102bb0debe5070402b6b6aa3153997
theterminal <kirilk777@gmail.com>
2022-12-25 08:32:50 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/additional_06_fishland.py
+++ 20220109_02_E_First_Steps_In_Coding/additional_06_fishland.py
@@ -1,6 +1,7 @@
-# 20220111 - Python Code - Fish Land
-# judge url: https://judge.softuni.org/Contests/Practice/Index/1642#5
-# pastebin url: https://pastebin.com/qpt82JYu
+# 20220111 - Python - Programming Basics
+# additional 06 - Fish Land - judge: https://judge.softuni.org/Contests/Practice/Index/1642#5
+# pastebin: https://pastebin.com/qpt82JYu
+
 
 # user input
 price_mackerel_lv_per_kg = float(input())
@@ -9,6 +10,7 @@ bonito_kg = float(input())
 horse_mackerel_kg = float(input())
 mussels_kg = int(input())
 price_mussels_lv_per_kg = 7.50
+
 
 # calculations
 price_bonito_lv_per_kg = price_mackerel_lv_per_kg * 1.6
@@ -19,6 +21,7 @@ cost_horse_mackerel_lv = horse_mackerel_kg * price_horse_mackerel_lv_per_kg
 cost_mussels_lv = mussels_kg * price_mussels_lv_per_kg
 
 total_ordered_lv = cost_bonito_lv + cost_horse_mackerel_lv + cost_mussels_lv
+
 
 # output
 print(f'{total_ordered_lv:.2f}')
