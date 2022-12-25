53e1b7c3e317781fa91f145792e7e4b7e6fdd960
theterminal <kirilk777@gmail.com>
2022-12-25 08:32:30 +0200

    Refactored

--- 20220109_02_E_First_Steps_In_Coding/additional_03_celsius_to_fahrenheit.py
+++ 20220109_02_E_First_Steps_In_Coding/additional_03_celsius_to_fahrenheit.py
@@ -1,12 +1,15 @@
-# 20220111 - Python Code - Celsius to Fahrenheit Converter
-# judge url: https://judge.softuni.org/Contests/Practice/Index/1642#2
-# pastebin url: https://pastebin.com/iZz9aAfY
+# 20220111 - Python - Programming Basics
+# additional 03 - Celsius to Fahrenheit Converter - judge: https://judge.softuni.org/Contests/Practice/Index/1642#2
+# pastebin: https://pastebin.com/iZz9aAfY
+
 
 # user input
 degrees_celsius = float(input())
 
+
 # calculations
 degrees_fahrenheit = (degrees_celsius * 1.8) + 32
+
 
 # output
 print(f"{degrees_fahrenheit:.2f}")
