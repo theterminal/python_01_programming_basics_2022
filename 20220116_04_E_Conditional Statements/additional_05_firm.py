33274c525546854ca1b2e904fa6989d5f6af10aa
theterminal <kirilk777@gmail.com>
2022-12-26 09:02:52 +0200

    Refactored

--- 20220116_04_E_Conditional Statements/additional_05_firm.py
+++ 20220116_04_E_Conditional Statements/additional_05_firm.py
@@ -1,10 +1,14 @@
-# 20220117 - Python Code - Firm
+# 20220117 - Python - Programming Basics - Conditional Statements
+# additional 05 - Firm
+
 import math
+
 
 # user input
 hrs_needed = int(input())
 days_company_has = int(input())
 number_people_working_over_time = int(input())
+
 
 # calculations & results
 hrs_company_normal = days_company_has * 8 * 0.9
