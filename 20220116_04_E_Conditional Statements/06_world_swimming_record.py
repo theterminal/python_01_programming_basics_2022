a243a332ac1e8b129f2f28c4227da04b2a856a4e
theterminal <kirilk777@gmail.com>
2022-12-26 09:01:57 +0200

    Refactored

--- 20220116_04_E_Conditional Statements/06_world_swimming_record.py
+++ 20220116_04_E_Conditional Statements/06_world_swimming_record.py
@@ -1,10 +1,13 @@
-# 20220114 - Python Code - World Swimming Record
+# 20220114 - Python - Programming Basics - Conditional Statements
+# 06 - World Swimming Record
+
 
 # user input
 old_record_sec = float(input())
 distance_to_swim_m = float(input())
 time_for_1_m_sec = float(input())
 resistance_15_m_sec = 12.5
+
 
 # calculations & results
 time_for_distance = distance_to_swim_m * time_for_1_m_sec
