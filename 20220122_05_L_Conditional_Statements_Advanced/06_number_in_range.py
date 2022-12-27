dd2293c81e67e186bae6fd2d7803bcd7745484d4
theterminal <kirilk777@gmail.com>
2022-12-28 00:07:50 +0200

    Refactored

--- 20220122_05_L_Conditional_Statements_Advanced/06_number_in_range.py
+++ 20220122_05_L_Conditional_Statements_Advanced/06_number_in_range.py
@@ -1,7 +1,10 @@
-# 20220120 - Python Code - Number In Range
+# 20220120 - Python - Conditional Statements Advanced
+# 06 - Number In Range - judge: https://judge.softuni.org/Contests/Compete/Index/2415#5
+
 
 # user input
 number = float(input())
+
 
 # calculations & results
 if -100 <= number <= 100 and number != 0:
