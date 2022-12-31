60f72d41a54837da0993f460c1669d01e7e35b02
theterminal <kirilk777@gmail.com>
2022-12-31 07:19:47 +0200

    Refactored

--- 20220129 _07_L_For_Loop/L07_Notes/L07_note_01.py
+++ 20220129 _07_L_For_Loop/L07_Notes/L07_note_01.py
@@ -1,18 +1,21 @@
-# 20220129 - Python Code
+# 20220129 - Python - For Loop
+# Note 01
 
-# ------- example 1 ---------------------------
 
 print('\n----------------- Example 1 ---------------------\n')
+
+
 word = 'Bora Sam'
 
-for i in word:          # important!
+for i in word:
     print(i)
 
-for i in word:          # important!
+for i in word:
     print(word)
 
-# ------- example 2 ---------------------------
 
 print('\n----------------- Example 2 ---------------------\n')
+
+
 for i in "word":
     print(i, end=" ")
