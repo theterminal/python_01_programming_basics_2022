32ebe3951d6aa409ecf1cd7611507c0abfea102b
theterminal <kirilk777@gmail.com>
2022-12-31 07:23:09 +0200

    Refactored

---
+++ 20220129 _07_L_For_Loop/02_numbers_from_1_to_n_with_step_3.py
@@ -0,0 +1,8 @@
+# 20220128 - Python - For Loop
+# 02 - Numbers From 1 to n With Step 3 - judge: https://judge.softuni.org/Contests/Compete/Index/2417#1
+
+
+n = int(input())
+
+for i in range(1, n + 1, 3):
+    print(i)
