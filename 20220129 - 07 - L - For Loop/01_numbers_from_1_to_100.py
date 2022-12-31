d6dde09adadb88e9908ea04294729e6c604a55d2
theterminal <kirilk777@gmail.com>
2022-12-31 07:24:04 +0200

    Deleted

--- 20220129 - 07 - L - For Loop/01_numbers_from_1_to_100.py
+++
@@ -1,4 +0,0 @@
-# 20220128 - Python Code - Numbers From 1 to 100
-
-for i in range(1, 101):
-    print(i)
--- 20220129 - 07 - L - For Loop/02_numbers_from_1_to_n_with_step_3.py
+++
@@ -1,6 +0,0 @@
-# 20220128 - Python Code - Numbers From 1 to n With Step 3
-
-n = int(input())
-
-for i in range(1, n + 1, 3):
-    print(i)
