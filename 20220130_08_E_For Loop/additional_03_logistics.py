c06a6f14e6ab287a2cf4bcd2cbfe7048ff0f3ccb
theterminal <kirilk777@gmail.com>
2023-01-02 09:30:09 +0200

    Refactored

--- 20220130_08_E_For Loop/additional_03_logistics.py
+++ 20220130_08_E_For Loop/additional_03_logistics.py
@@ -1,12 +1,15 @@
-# 20220130 - Python Code
-# Logistics
+# 20220130 - Python - For Loop
+# Additional 03 - Logistics - judge: https://judge.softuni.org/Contests/Practice/Index/1680#2
+
 
 # user's input
 number_loads = int(input())
 
+
 # additional elements
 tons_bus = tons_truck = tons_train = 0
 paid_bus = paid_truck = paid_train = 0
+
 
 # calculations
 for i in range(number_loads):
@@ -29,6 +32,7 @@ average_price_per_ton = total_paid / total_tons
 percent_tons_bus = tons_bus / total_tons * 100
 percent_tons_truck = tons_truck / total_tons * 100
 percent_tons_train = tons_train / total_tons * 100
+
 
 # result
 print(f'{average_price_per_ton:.2f}')
--- 20220130_08_E_For Loop/additional_04_grades.py
+++ 20220130_08_E_For Loop/additional_04_grades.py
@@ -1,13 +1,16 @@
-# 20220130 - Python Code
-# Grades
+# 20220130 - Python - For Loop
+# Additional 04 - Grades - judge: https://judge.softuni.org/Contests/Practice/Index/1680#3
+
 
 # user's input
 num_students = int(input())
+
 
 # additional elements
 grade_200_299 = grade_300_399 = grade_400_499 = grade_500_600 = 0
 num_students_200_299 = num_students_300_399 = num_students_400_499 = num_students_500_600 = 0
 sum_grades = 0
+
 
 # calculations
 for i in range(num_students):
@@ -33,6 +36,7 @@ students_300_399 = num_students_300_399 / num_students * 100
 students_200_299 = num_students_200_299 / num_students * 100
 sum_grades = grade_200_299 + grade_300_399 + grade_400_499 + grade_500_600
 average_grade = sum_grades / num_students
+
 
 # result
 print(f"Top students: {students_500_600:.2f}%")
