5e580a468355f7a3d8e54f4a8ac269c9f67360b5
theterminal <kirilk777@gmail.com>
2023-01-03 08:45:21 +0200

    Refactored

--- 20220206_10_E_While Loop/additional_05_average_number.py
+++ 20220206_10_E_While Loop/additional_05_average_number.py
@@ -1,12 +1,15 @@
-# 20220206 - Python Code
-# Average Numbers
+# 20220206 - Python - While Loop
+# Additional 05 - Average Numbers - judge: https://judge.softuni.org/Contests/Practice/Index/1684#4
+
 
 # user's input
 numbers_entered = int(input())
 
+
 # additional elements
 total = 0
 number = ""
+
 
 # result
 for i in range(1, numbers_entered + 1):
