0976a0e3bd3d96a901f69f86586b675044d40581
theterminal <kirilk777@gmail.com>
2023-01-03 08:45:16 +0200

    Refactored

---
+++ 20220206_10_E_While Loop/additional_04_numbers_divided_by_3_without_reminder.py
@@ -0,0 +1,8 @@
+# 20220206 - Python - While Loop
+# Additional 04 - Numbers Divided by 3 Without Reminder - judge: https://judge.softuni.org/Contests/Practice/Index/1684#3
+
+
+# result
+for i in range(1, 101):
+    if i % 3 == 0:
+        print(i)
