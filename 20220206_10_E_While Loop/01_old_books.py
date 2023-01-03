294851a816e22751df0acda09ea364fdd5b1a259
theterminal <kirilk777@gmail.com>
2023-01-03 08:43:45 +0200

    Refactored

---
+++ 20220206_10_E_While Loop/01_old_books.py
@@ -0,0 +1,48 @@
+# 20220205 - Python - While Loop
+# 01 - Old Books - judge: https://judge.softuni.org/Contests/Compete/Index/2420#0
+
+
+# -------------------- version 1 ----------------------------
+
+
+# user input
+book_searched_for = input()
+
+
+# calculations & results
+counter_books = 0
+
+while True:
+    book = input()
+
+    if book == "No More Books":
+        print(f"The book you search is not here!")
+        print(f"You checked {counter_books} books.")
+        break
+    else:
+        counter_books += 1
+
+        if book == book_searched_for:
+            print(f"You checked {counter_books - 1} books and found it.")
+            break
+
+
+# -------------------- version 2 ----------------------------
+
+
+# user input
+book_searched = input()
+book_in_library = input()
+
+# calculations & results
+n = 0
+
+while book_in_library != "No More Books":
+    if book_searched == book_in_library:
+        print(f"You checked {n} books and found it.")
+        quit()
+    n += 1
+    book_in_library = input()
+
+print("The book you search is not here!")
+print(f"You checked {n} books.")
