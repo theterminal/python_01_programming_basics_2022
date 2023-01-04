2be4175e411d42d2ae57e514508947a9d3288fb8
theterminal <kirilk777@gmail.com>
2023-01-04 07:50:09 +0200

    Refactored

---
+++ 20220212_11_L_Nested Loops/01_clock.py
@@ -0,0 +1,8 @@
+# 20220208 - Python - Nested Loops
+# 01 - Clock - judge: https://judge.softuni.org/Contests/Compete/Index/2421#0
+
+
+# calculations & results
+for h in range(24):
+    for m in range(60):
+        print(f"{h}:{m}")
