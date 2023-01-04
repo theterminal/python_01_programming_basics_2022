4f03be8d60ee84fdb80a145323db3af4ee6b44b8
theterminal <kirilk777@gmail.com>
2023-01-04 07:49:49 +0200

    Deleted

--- 20220212 - 11 - L - Nested Loops/01_clock.py
+++
@@ -1,7 +0,0 @@
-# 20220208 - Python Code
-# Clock
-
-# calculations & results
-for h in range(24):
-    for m in range(60):
-        print(f"{h}:{m}")
--- 20220212 - 11 - L - Nested Loops/02_multiplication_table.py
+++
@@ -1,6 +0,0 @@
-# 20220208 - Python Code - Multiplication Table
-
-# calculations & results
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f"{i} * {j} = {i * j}")
