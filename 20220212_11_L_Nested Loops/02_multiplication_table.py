5b7ce556eb4bbf3e56f9be60df74a619348e6628
theterminal <kirilk777@gmail.com>
2023-01-05 08:36:13 +0200

    Refactored

---
+++ 20220212_11_L_Nested Loops/02_multiplication_table.py
@@ -0,0 +1,8 @@
+# 20220208 - Python - Nested Loops
+# 02 - Multiplication Table - judge: https://judge.softuni.org/Contests/Compete/Index/2421#1
+
+
+# calculations & results
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} * {j} = {i * j}")
