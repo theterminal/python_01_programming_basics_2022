8f42a3fb8c91ce00923df68fe53d026926cf82b6
theterminal <kirilk777@gmail.com>
2023-01-07 09:02:28 +0200

    Refactored

--- 20220219_14_Python_Pre-Exam/01_excursion.py
+++ 20220219_14_Python_Pre-Exam/01_excursion.py
@@ -1,4 +1,6 @@
-# 20220219 - Python Code - Excursion - judge url: https://judge.softuni.org/Contests/Compete/Index/3351#0
+# 20220219 - Python - Programming Basics - Pre-Exam
+# 01 - Excursion - judge: https://judge.softuni.org/Contests/Compete/Index/3351#0
+
 
 # user information
 num_people_in_the_group_entered = int(input())
@@ -8,6 +10,7 @@ num_tickets_museum = int(input())
 cost_per_night_per_person = 20
 cost_per_card_per_person = 1.60
 cost_per_ticket_museum_per_person = 6
+
 
 # calculations & results
 cost_per_person_for_nights = num_nights_entered * cost_per_night_per_person
