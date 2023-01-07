685fb0e805978db267d6fe135c0055349f3f747d
theterminal <kirilk777@gmail.com>
2023-01-07 09:04:12 +0200

    Refactored

--- Python_Older_Exams_20200718/02_add_bags.py
+++ Python_Older_Exams_20200718/02_add_bags.py
@@ -1,10 +1,13 @@
-# 20220218 - Python Code - Add Bags
+# 20220218 - Python - Programming Basics - Exam 20200718
+# 02 - Add Bags - judge: https://judge.softuni.org/Contests/Practice/Index/2507#1
+
 
 # user's input
 price_luggage_over_20_kg = float(input())
 luggage_kg = float(input())
 days_to_travel = int(input())
 number_bags = int(input())
+
 
 # calculations
 if luggage_kg < 10:
@@ -22,6 +25,7 @@ elif days_to_travel < 7:
     fee_to_pay *= 1.4
 
 fee_to_pay *= number_bags
+
 
 # result
 print(f"The total price of bags is: {fee_to_pay:.2f} lv.")
